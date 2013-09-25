76b52732c710b81deedc873299701d2d618f44db
John <jgiagnorio@gmail.com>
2013-09-25 17:39:56 -0400

    Add user microposts

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -23,6 +23,9 @@ describe "Authentication" do
 				before { click_link "Home" }
 				it { should_not have_selector('div.alert.alert-error') }
 			end
+
+			it { should_not have_link('Profile') }
+			it { should_not have_link('Settings') }
 		end
 
 		describe "with valid information" do
@@ -73,6 +76,19 @@ describe "Authentication" do
 				describe "visiting the user index" do
 					before{ visit users_path }
 					it { should have_title('Sign in') }
+				end
+			end
+
+			describe "in the Microposts controller" do
+
+				describe "submitting to the create action" do
+					before { post microposts_path }
+					specify { expect(response).to redirect_to(signin_path) }
+				end
+
+				describe "submitting to the destroy action" do
+					before { delete micropost_path(FactoryGirl.create(:micropost)) }
+					specify { expect(response).to redirect_to(signin_path) }
 				end
 			end
 		end
